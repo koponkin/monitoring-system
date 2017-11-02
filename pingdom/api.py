0aab26e1f941b231a86539e16fd658dc40d8033a
koponkin <koponkin@gmail.com>
2017-11-02 21:56:55 +0200

    add Pingdom metric service

---
+++ pingdom/api.py
@@ -0,0 +1,35 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+
+class ApiError(Exception):
+
+    def __init__(self, http_response):
+        content = json.loads(http_response.content)
+        self.status_code = http_response.status_code
+        self.status_desc = content['error']['statusdesc']
+        self.error_message = content['error']['errormessage']
+        super(ApiError, self).__init__(self.__str__())
+
+    def __repr__(self):
+        return 'pingdom.ApiError: HTTP `%s - %s` returned with message, "%s"' % \
+               (self.status_code, self.status_desc, self.error_message)
+
+    def __str__(self):
+        return self.__repr__()
+
+
+class Api(object):
+
+    def __init__(self, username, password, apikey, version="2.1"):
+        self.base_url = "https://api.pingdom.com/api/" + version + "/"
+        self.auth = HTTPBasicAuth(username, password)
+        self.headers = {'App-Key': apikey}
+
+    def send(self, method, resource, resource_id="", data={}, params={}):
+        response = requests.request(method, self.base_url + resource + "/" + str(resource_id), auth=self.auth, headers=self.headers, data=data, params=params)
+        if response.status_code != 200:
+            raise ApiError(response)
+        else:
+            return response.json()
---
+++ pingdom/pingdomClient.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta, timezone
+
+from pingdom.api import Api
+from pingdom.metric import CheckResult, Metric
+
+
+class PingdomClient:
+    def __init__(self, username, password, apikey, api_version='2.0'):
+        """
+        :param username: account main email
+        :param password: account password
+        :param apikey: Pingdom api key
+        """
+        self.username = username
+        self.password = password
+        self.apikey = apikey
+        self.api = Api(username, password, apikey, api_version)
+        self.checks = {}
+        for item in self.api.send('get', "checks")['checks']:
+            self.checks[item["name"].lower()] = item['id']
+
+    def get_server_time(self):
+        servertime_timestamp = self.api.send("get", "servertime")["servertime"]
+        return datetime.fromtimestamp(servertime_timestamp)
+
+    def get_metrics(self, period):
+        metrics = []
+        from_time = datetime.now() - timedelta(minutes=period)
+        from_timestamp = from_time.timestamp();
+        params = {"includeanalysis": "true", "from": from_timestamp}
+        for check_name, check_id in self.checks.items():
+
+            result_response = self.api.send("get", "results", check_id, {}, params)['results']
+            check_results = []
+            for metric in result_response:
+                responsetime = None
+                if "responsetime" in metric:
+                    responsetime = metric["responsetime"]
+                result = CheckResult(metric["time"], metric["status"], responsetime, metric["statusdesc"])
+                check_results.append(result)
+            metrics.append(Metric(check_name, check_results))
+
+        return metrics
+
